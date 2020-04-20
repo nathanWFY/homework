94f0924218f3d316e401ba2cdb4602784063643c
nathanWFY <63992856+nathanWFY@users.noreply.github.com>
2020-04-20 12:21:21 -0500

    Update fit_neuralnet.R

--- R/fit_neuralnet.R
+++ R/fit_neuralnet.R
@@ -221,12 +221,13 @@ rprop <-
                           act.fct = act.fct, act.deriv.fct = act.deriv.fct, output.act.fct = output.act.fct, 
                           output.act.deriv.fct = output.act.deriv.fct, special, output.special)
     err.deriv <- err.deriv.fct(result$net.result, response)
-    print("wangsheng5")
-    print(err.deriv)
     gradients <- calculate.gradients(weights = weights, length.weights = length.weights, 
                                      neurons = result$neurons, neuron.deriv = result$neuron.deriv, 
                                      err.deriv = err.deriv, exclude = exclude, linear.output = linear.output)
     reached.threshold <- max(abs(gradients))
+    print("wangsheng5")
+    print(err.deriv)
+    print(gradients)
     min.reached.threshold <- reached.threshold
     while (step < stepmax && reached.threshold > threshold) {
       if (!is.character(lifesign) && step%%lifesign.step == 
