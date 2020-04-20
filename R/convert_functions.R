187ace544e7000f17ef8093590a207b6ee2e7506
nathanWFY <63992856+nathanWFY@users.noreply.github.com>
2020-04-20 12:22:18 -0500

    Update convert_functions.R

--- R/convert_functions.R
+++ R/convert_functions.R
@@ -46,10 +46,6 @@ convert.error.function <- function(fun) {
     }
     attr(fct, "type") <- "ce"
     deriv.fct <- function(x, y) {
-      print("wangsheng3")
-      print(x)
-      print("wangsheng4")
-      print(y)
       print("wangsheng6")
       print((1 - y)/max((1 - x), 10^-323) - y/max(x, 10^-323))
      (1 - y)/max((1 - x), 10^-323) - y/max(x, 10^-323)
