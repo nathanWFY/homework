e16cef58aed5d9a1ab5a49b56cb7fec8da127f56
nathanWFY <63992856+nathanWFY@users.noreply.github.com>
2020-04-20 12:20:34 -0500

    Update fit_neuralnet.R

--- R/fit_neuralnet.R
+++ R/fit_neuralnet.R
@@ -220,10 +220,6 @@ rprop <-
     result <- compute.net(weights, length.weights, covariate = covariate, 
                           act.fct = act.fct, act.deriv.fct = act.deriv.fct, output.act.fct = output.act.fct, 
                           output.act.deriv.fct = output.act.deriv.fct, special, output.special)
-    print("wangsheng1")
-    print(result$net.result)
-    print("wangsheng2")
-    print(response)
     err.deriv <- err.deriv.fct(result$net.result, response)
     print("wangsheng5")
     print(err.deriv)
